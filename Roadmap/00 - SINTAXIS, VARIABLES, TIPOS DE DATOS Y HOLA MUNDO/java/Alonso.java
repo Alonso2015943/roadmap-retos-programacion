dafc053418906cbb97bafcbce7d6a228d425c1ab
Alonso2015943 <jairorios2024@outlook.com>
2024-11-25 21:16:59 -0500

    Create Alonso.java

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/java/Alonso.java
@@ -0,0 +1,22 @@
+// https://www.java.com/es/
+
+// sintaxis para crear comentario en una linea
+
+/**
+sintaxis para 
+crear comentarios
+en varias lineas
+*/
+{
+final String CONSTANTE = "Esto es una constante";
+
+String myVariAble = "JAVA";
+int myVariable = 1;
+short numero = 0;
+long largo = 1234567;
+float flotante = 1.5;
+boolean booleano = true;
+boolean booleano = false;
+
+System.out.println("¡Hola, JAVA");
+}
